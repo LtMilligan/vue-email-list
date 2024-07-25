9c516099dd3652622310880f65968e8945464f25
LtMilligan <gioele.miscia@gmail.com>
2024-07-25 15:55:11 +0200

    [UPD] Adding var for randomMail and function for generate 10 times random mail

--- js/script.js
+++ js/script.js
@@ -3,10 +3,16 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            randomMail: null
         }
     },
     methods: {
-        
+        generateRandomMail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((resp) => {
+                    this.randomMail = resp.data.response
+                });
+            }
+        }
     },
 })
